9e51a0845bd76468b60dd466da86ecff23dc813f
BrenesRi <ribrenes10@gmail.com>
2022-05-09 19:24:18 -0600

    Commit de prueba

---
+++ out/build/x64-Debug/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ribre/Downloads/lab04-v2-dataflow-XarthDaichi-main/lib/googletest
+# Build directory: C:/Users/ribre/Downloads/lab04-v2-dataflow-XarthDaichi-main/out/build/x64-Debug/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ out/build/x64-Debug/lib/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ribre/Downloads/lab04-v2-dataflow-XarthDaichi-main/lib/googletest/googletest
+# Build directory: C:/Users/ribre/Downloads/lab04-v2-dataflow-XarthDaichi-main/out/build/x64-Debug/lib/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
